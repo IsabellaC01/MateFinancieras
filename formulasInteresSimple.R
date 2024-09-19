31e9f3e5f1c1abd316a674401818686d993d5512
IsabellaC01 <isabellacampos228@gmail.com>
2024-09-19 12:41:00 -0600

    Carga tarea V1.0
    En esta carga se sube la primera versión de la tarea número 1

---
+++ formulasInteresSimple.R
@@ -0,0 +1,12 @@
+# Se presentan las funciones de interés simple.
+# Autor: Isabella Campos Ramírez.
+# V 1.0: 19 de septiembre de 2024.
+
+# Valor futuro con interés simple:
+
+valorFinalSimple=function(VA,r,t){
+  xSalida=VA*(1+(r*t))
+  
+  return(xSalida)
+}
+
